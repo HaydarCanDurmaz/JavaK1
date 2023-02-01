567f1bd12c1485728e958ebc430131ec4fc50d83
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-02-01 17:09:12 +0300

    task 1 yapıldı

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -4,9 +4,5 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
 
-        System.out.println("task 2");
-
-
-        System.out.println("task 3");
     }
 }
